aee9b5d06941d62c45bb65467af8eb5e1cafc39c
cn-wx <121894598@qq.com>
2019-02-09 23:00:00 +0800

    update framework & slide 1

--- app/src/javascripts/main.js
+++ app/src/javascripts/main.js
@@ -6,63 +6,76 @@
 
 
     $(document).ready(function () {
-        var bgMusic = $('audio').get(0);
-        var $btnMusic = $('.btn-music');
+        /***********************************************
+         *                   Loader                    *
+         ***********************************************/
+        var loader = new PxLoader();
         var $upArrow = $('.up-arrow');
+        // Images
+        // var BASE_IMAGE_PATH = '/dist/images/';
+        var BASE_IMAGE_PATH = '/images/';
+        var fileList = [
+            'logo.png',
+            'slide_1_background.png',
+            'slide_1_title.png',
+            'slide_1_text.png',
+            
+        ];
+        for(var i = 0; i < fileList.length; i++){
+            var pxImage = new PxLoaderImage(BASE_IMAGE_PATH + fileList[i]);
+            pxImage.imageNumber = i + 1;
+            loader.add(pxImage);
+        }
+        loader.addCompletionListener(function(){
+            console.log("预加载图片："+fileList.length+"张");
+            
+            // init Swiper
+            new Swiper('.swiper-container', {
+                mousewheelControl: true,
+                effect: 'coverflow',    // slide, fade, coverflow or flip
+                speed: 400,
+                direction: 'vertical',
+                fade: {
+                    crossFade: false
+                },
+                coverflow: {
+                    rotate: 100,
+                    stretch: 0,
+                    depth: 300,
+                    modifier: 1,
+                    slideShadows: false     // do disable shadows for better performance
+                },
+                flip: {
+                    limitRotation: true,
+                    slideShadows: false     // do disable shadows for better performance
+                },
+                onInit: function (swiper) {
+                    animationControl.initAnimationItems();  // get items ready for animations
+                    animationControl.playAnimation(swiper); // play animations of the first slide
+                },
+                onTransitionStart: function (swiper) {     // on the last slide, hide .btn-swipe
+                    if (swiper.activeIndex === swiper.slides.length - 1) {
+                        $upArrow.hide();
+                    } else {
+                        $upArrow.show();
+                    }
+                },
+                onTransitionEnd: function (swiper) {       // play animations of the current slide
+                    animationControl.playAnimation(swiper);
+                },
+            });
 
-        // background music control
-        $btnMusic.click(function () {
-            if (bgMusic.paused) {
-                bgMusic.play();
-                $(this).removeClass('paused');
-            } else {
-                bgMusic.pause();
-                $(this).addClass('paused');
-            }
+            // hide loading animation since everything is ready
+            $(".loading-overlay").slideUp();
         });
 
-        // init Swiper
-        new Swiper('.swiper-container', {
-            mousewheelControl: true,
-            effect: 'coverflow',    // slide, fade, coverflow or flip
-            speed: 400,
-            direction: 'vertical',
-            fade: {
-                crossFade: false
-            },
-            coverflow: {
-                rotate: 100,
-                stretch: 0,
-                depth: 300,
-                modifier: 1,
-                slideShadows: false     // do disable shadows for better performance
-            },
-            flip: {
-                limitRotation: true,
-                slideShadows: false     // do disable shadows for better performance
-            },
-            onInit: function (swiper) {
-                animationControl.initAnimationItems();  // get items ready for animations
-                animationControl.playAnimation(swiper); // play animations of the first slide
-            },
-            onTransitionStart: function (swiper) {     // on the last slide, hide .btn-swipe
-                if (swiper.activeIndex === swiper.slides.length - 1) {
-                    $upArrow.hide();
-                } else {
-                    $upArrow.show();
-                }
-            },
-            onTransitionEnd: function (swiper) {       // play animations of the current slide
-                animationControl.playAnimation(swiper);
-            },
-            onTouchStart: function (swiper, event) {    // mobile devices don't allow audios to play automatically, it has to be triggered by a user event(click / touch).
-                if (!$btnMusic.hasClass('paused') && bgMusic.paused) {
-                    bgMusic.play();
-                }
-            }
+        loader.addProgressListener(function(e){
+            var percent = Math.round( (e.completedCount / e.totalCount) * 100);
+            $(".loadingProgress2").css({"width":percent+"%"});
+            $(".progressNumb").html(percent + "%");
         });
 
-        // hide loading animation since everything is ready
-        $('.loading-overlay').slideUp();
+        loader.start();
+        /***********************************************/
     });
 })();
